09a9ea5e64fd14148e0385eb836ee4e8953c0102
Mate Radnai <mate_radnai@epam.com>
2022-08-31 15:53:07 +0200

    move ajv schema config into separate file

--- specs/json-schemas/ajv-instance.js
+++ specs/json-schemas/ajv-instance.js
@@ -1,7 +1,8 @@
 const Ajv = require("ajv");
 const addFormats = require("ajv-formats");
+const config = require("./json-schema.config.json");
 
-const ajvInstance = new Ajv({ allErrors: true});
+const ajvInstance = new Ajv(config);
 addFormats(ajvInstance);
 
 module.exports = ajvInstance;
