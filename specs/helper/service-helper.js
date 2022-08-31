ef7d24e5b1204cce718021c68010f4d208e51c28
Mate Radnai <mate_radnai@epam.com>
2022-08-31 15:55:19 +0200

    cleanup and refactor

--- specs/helper/service-helper.js
+++ specs/helper/service-helper.js
@@ -2,12 +2,23 @@ const axios = require("axios");
 const url = require("url");
 const apiKey = process.env.NASA_API_KEY;
 
-async function getApodRequest(payload, authenticated) {
+/**
+ * Create get request for Apod
+ *
+ * @param  {String} payload request parameters
+ * @param  {boolean} isAuthenticatedSession true if API key is provided
+ * @return response of the Apod API
+ */
+async function getApodRequest(payload, isAuthenticatedSession) {
     const params = new url.URLSearchParams(payload);
-    if (authenticated === true) {
-        params.append("api_key", apiKey);
+    if (isAuthenticatedSession === true) {
+        if (apiKey) {
+            params.append("api_key", apiKey);
+        } else {
+            console.error("NASA_API_KEY Secret is missing!");
+        }
     }
-    //console.log("Request: ", `https://api.nasa.gov/planetary/apod?${params}`);
+    // console.debug("Request: ", `https://api.nasa.gov/planetary/apod?${params}`);
     const res = await axios.get(`https://api.nasa.gov/planetary/apod?${params}`);
 
     return res;
